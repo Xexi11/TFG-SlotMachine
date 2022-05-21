2bf60f7ec8063a36618f4615bf91b82f0824d1f8
Xexi11 <sergialsinacaldera@gmail.com>
2022-05-21 11:38:38 +0200

    Firebase

--- slotmachineinis/src/App.js
+++ slotmachineinis/src/App.js
@@ -1,45 +1,63 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import './App.css';
-import Home from './components/pages/Home';
-import { BrowserRouter as Router, Switch, Route, useHistory } from 'react-router-dom';
-import Profile from './components/pages/Profile';
-import SignUp from './components/pages/SignUp';
-import Games from './components/pages/Games';
-import { useAuth0, User } from '@auth0/auth0-react';
+import React from "react";
+import Navbar from "./components/Navbar";
+import "./App.css";
+import Home from "./components/pages/Home";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useHistory,
+} from "react-router-dom";
+import Profile from "./components/pages/Profile";
+import SignUp from "./components/pages/SignUp";
+import Games from "./components/pages/Games";
+import { useAuth0, User } from "@auth0/auth0-react";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 import { useEffect } from "react";
 
-import SlotMachine_Page from './components/pages/SlotMachine_Page';
-import SlotMachine from './components/SlotMachine';
+import SlotMachine_Page from "./components/pages/SlotMachine_Page";
+import SlotMachine from "./components/SlotMachine";
 
 function App() {
   const history = useHistory();
-  const {isAuthenticated} = useAuth0();
+  const { isAuthenticated } = useAuth0();
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyDVbrZLKrei2XCCaTqA5JE_PmPUETbEbF4",
+    authDomain: "slotmachine-c537c.firebaseapp.com",
+    projectId: "slotmachine-c537c",
+    storageBucket: "slotmachine-c537c.appspot.com",
+    messagingSenderId: "484508762054",
+    appId: "1:484508762054:web:5e9bc1fe013721d8687cc7",
+    measurementId: "G-X07R5E819H",
+  };
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
 
   return (
     <>
       <Router>
         <Navbar />
         <Switch>
-        {isAuthenticated ? (
-          <Switch>
-          <Route path='/' exact component={Home} />
-        
-          <Route path='/profile' component={Profile} />
-          <Route path='/games' component={Games} />
-          <Route path='/singup' component={SignUp} />
-          <Route path='/slotmachine' component={SlotMachine} />
-          </Switch>
-        ): (
-          <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/profile' component={SignUp} />
-          <Route path='/games' component={SignUp} />
-          </Switch>
-        )}
-          </Switch> 
-      
+          {isAuthenticated ? (
+            <Switch>
+              <Route path="/" exact component={Home} />
+
+              <Route path="/profile" component={Profile} />
+              <Route path="/games" component={Games} />
+              <Route path="/singup" component={SignUp} />
+              <Route path="/slotmachine" component={SlotMachine} />
+            </Switch>
+          ) : (
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/profile" component={SignUp} />
+              <Route path="/games" component={SignUp} />
+            </Switch>
+          )}
+        </Switch>
       </Router>
     </>
   );
--- slotmachineinis/src/components/pages/SignUp.js
+++ slotmachineinis/src/components/pages/SignUp.js
@@ -1,16 +1,38 @@
-import { useAuth0 } from '@auth0/auth0-react';
-import React from 'react';
-import { Button } from '../Button';
-import '../Css/SingUp.css';
-import '../../App.css';
+import { useAuth0 } from "@auth0/auth0-react";
+import React from "react";
+import { Button } from "../Button";
+import "../Css/SingUp.css";
+import "../../App.css";
 export default function SignUp() {
-  const { loginWithRedirect} = useAuth0();
+  var firebase = require("firebase");
+  var firebaseui = require("firebaseui");
+  var ui = new firebaseui.auth.AuthUI(firebase.auth());
+  ui.start("#firebaseui-auth-container", {
+    signInOptions: [
+      {
+        provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD,
+        requireDisplayName: false,
+      },
+    ],
+  });
+
+  const { loginWithRedirect } = useAuth0();
   return (
-    <div className= "singup">
-        <div className="signup-box">
-          <h1>Inicia sesión para acceder a Inis Casino </h1>
-          <Button id="bttn_login" onClick={() => loginWithRedirect()} buttonStyle='btn--outline'buttonSize='medium'> SING IN </Button>
-        </div>
+    <div className="singup">
+      <div className="signup-box">
+        <h1>Inicia sesión para acceder a Inis Casino </h1>
+        <Button
+          id="bttn_login"
+          onClick={() => loginWithRedirect()}
+          buttonStyle="btn--outline"
+          buttonSize="medium"
+        >
+          {" "}
+          SING IN{" "}
+        </Button>
+      </div>
     </div>
   );
 }
